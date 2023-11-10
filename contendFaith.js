540233c4dadf7f6ecae62bdda19a2b5f8ad699b0
mercysound <adekunleadewolemercy@gmail.com>
2023-11-10 04:54:08 +0100

    faith

--- contendFaith.js
+++ contendFaith.js
@@ -1,6 +1,11 @@
 const myModal =  bootstrap.Modal.getOrCreateInstance('#booking-modal');
+const myModal2 =  bootstrap.Modal.getOrCreateInstance('#modal2');
+const myModal3 =  bootstrap.Modal.getOrCreateInstance('#modal3');
+const myModal4 =  bootstrap.Modal.getOrCreateInstance('#modal4');
+const myModal5 =  bootstrap.Modal.getOrCreateInstance('#modal5');
+const myModal6 =  bootstrap.Modal.getOrCreateInstance('#modal6');
 var diceSound = new Audio('./cf/dice_ass/Rolling-Dice-Sound-effect1.mp3');
-var diceRandNoPlayerOnePlayerOne = 1;
+var diceRandNoPlayerOnePlayerOne;
 
 
 var arrayofTablesId = {
@@ -62,21 +67,26 @@ const dispLuck = ()=>{
     dispDiceRolOne.innerHTML = `<img src='./cf/dice_ass/perspective-dice-six-faces-one.png' style="width: 130px; height: 100px;">`
     // alert('im work')
     // for1.innerHTML = `<img src='./cf/dice_ass/perspective-dice-six-faces-one.png' style="width: 130px; height: 100px;">`
-    // myModal.show()
-  }else if(diceRandNoPlayerOne == 2){
+    myModal.show()
+  }else if(diceRandNoPlayerOnePlayerOne == 2){
     dispDiceRolOne.innerHTML = `<img src='./cf/dice_ass/perspective-dice-six-faces-two.png' style="width: 130px; height: 100px;">`
-
-  }else if(diceRandNoPlayerOne == 3){
+    myModal2.show()
+  }else if(diceRandNoPlayerOnePlayerOne == 3){
     dispDiceRolOne.innerHTML = `<img src='./cf/dice_ass/perspective-dice-six-faces-three.png' style="width: 130px; height: 100px;">`
-
-  }else if(diceRandNoPlayerOne == 4){
+    myModal3.show()
+    
+    
+  }else if(diceRandNoPlayerOnePlayerOne == 4){
     dispDiceRolOne.innerHTML = `<img src='./cf/dice_ass/perspective-dice-six-faces-four.png' style="width: 130px; height: 100px;">`
+    myModal4.show()
     
-  }else if(diceRandNoPlayerOne == 5){
+  }else if(diceRandNoPlayerOnePlayerOne == 5){
     dispDiceRolOne.innerHTML = `<img src='./cf/dice_ass/perspective-dice-six-faces-five.png' style="width: 130px; height: 100px;">`
+    myModal5.show()
     
-  }else if(diceRandNoPlayerOne == 6){
+  }else if(diceRandNoPlayerOnePlayerOne == 6){
     dispDiceRolOne.innerHTML = `<img src='./cf/dice_ass/perspective-dice-six-faces-six.png' style="width: 130px; height: 100px;">`
+    myModal6.show()
     
   }
 };
@@ -92,13 +102,13 @@ const dispLuck = ()=>{
   const rolDicePlayerOne = ()=>{
   dispDiceRolOne.innerHTML = `<img style="width: 130px; height: 100px;" src='./cf/dice_ass/dice.gif'>`
   diceSound.play();
-  // diceRandNoPlayerOne = Math.floor((Math.random() * 6 + 1)); ////
-  // diceRandNoPlayerOnePlayerOne = diceRandNoPlayerOne
-  diceRandNoPlayerOnePlayerOne = 1
+  diceRandNoPlayerOne = Math.floor((Math.random() * 6 + 1)); ////
+  diceRandNoPlayerOnePlayerOne = 5
+  // diceRandNoPlayerOnePlayerOne = 6
   setTimeout(function(){
     dispLuck()  
     diceSound.pause();
-    myModal.show()
+    // myModal.show()
   }, 1200);
   // var backgroundToBlink = document.getElementById('for40')
   // const colors = ['#ff0000', 'black', '#0000ff'];
@@ -110,7 +120,7 @@ const dispLuck = ()=>{
 
   const blinkBackground = ()=>{
     if(diceRandNoPlayerOnePlayerOne == 1){
-      let recentImg = document.getElementById('forOne1')
+      let recentImg = document.getElementById('forPicOne')
       recentImg.style.display = 'none'
       arrayofTablesId.for1.style.background = `linear-gradient( 120deg, rgba(180, 212, 26, ${colors[currentColorIndex]}), rgba(212, 23, 23, ${colors[currentColorIndex]}), rgba(29, 19, 19, ${colors[currentColorIndex]})),
       url("cf/1.JPG")`
@@ -118,15 +128,45 @@ const dispLuck = ()=>{
       arrayofTablesId.for1.style.backgroundSize = `100% 100%`
       // arrayofTablesId.for1.style.backgroundColor = colors[currentColorIndex]
     }else if(diceRandNoPlayerOnePlayerOne == 2){
-      for2.style.backgroundColor = colors[currentColorIndex]
+      let recentImg2 = document.getElementById('forPicTwo')
+      recentImg2.style.display = 'none'
+      arrayofTablesId.for2.style.background = `linear-gradient( 120deg, rgba(15, 15, 15, ${colors[currentColorIndex]}), rgba(212, 23, 23, ${colors[currentColorIndex]}), rgba(29, 19, 19, ${colors[currentColorIndex]})),
+      url("cf/2.JPG")`
+      arrayofTablesId.for2.style.backgroundRepeat = `no-repeat`
+      arrayofTablesId.for2.style.backgroundSize = `100% 100%`
+      // arrayofTablesId.for2.style.backgroundColor = colors[currentColorIndex]
     }else if(diceRandNoPlayerOnePlayerOne == 3){
-      for3.style.backgroundColor = colors[currentColorIndex]
+      let recentImg3 = document.getElementById('forPicThree')
+      recentImg3.style.display = 'none'
+      arrayofTablesId.for3.style.background = `linear-gradient( 120deg, rgba(15, 15, 15, ${colors[currentColorIndex]}), rgba(212, 23, 23, ${colors[currentColorIndex]}), rgba(29, 19, 19, ${colors[currentColorIndex]})),
+      url("cf/3.JPG")`
+      arrayofTablesId.for3.style.backgroundRepeat = `no-repeat`
+      arrayofTablesId.for3.style.backgroundSize = `100% 100%`
+      // for3.style.backgroundColor = colors[currentColorIndex]
     }else if(diceRandNoPlayerOnePlayerOne == 4){
-      for4.style.backgroundColor = colors[currentColorIndex]
+      let recentImg4 = document.getElementById('forPicFour')
+      recentImg4.style.display = 'none'
+      arrayofTablesId.for4.style.background = `linear-gradient( 120deg, rgba(15, 15, 15, ${colors[currentColorIndex]}), rgba(212, 23, 23, ${colors[currentColorIndex]}), rgba(29, 19, 19, ${colors[currentColorIndex]})),
+      url("cf/4.JPG")`
+      arrayofTablesId.for4.style.backgroundRepeat = `no-repeat`
+      arrayofTablesId.for4.style.backgroundSize = `100% 100%`
+      // for4.style.backgroundColor = colors[currentColorIndex]
     }else if(diceRandNoPlayerOnePlayerOne == 5){
-      for5.style.backgroundColor = colors[currentColorIndex]
+      let recentImg5 = document.getElementById('forPicFive')
+      recentImg5.style.display = 'none'
+      arrayofTablesId.for5.style.background = `linear-gradient( 120deg, rgba(15, 15, 15, ${colors[currentColorIndex]}), rgba(212, 23, 23, ${colors[currentColorIndex]}), rgba(29, 19, 19, ${colors[currentColorIndex]})),
+      url("cf/5.JPG")`
+      arrayofTablesId.for5.style.backgroundRepeat = `no-repeat`
+      arrayofTablesId.for5.style.backgroundSize = `100% 100%`
+      // for5.style.backgroundColor = colors[currentColorIndex]
     }else if(diceRandNoPlayerOnePlayerOne == 6){
-      for6.style.backgroundColor = colors[currentColorIndex]
+      let recentImg6 = document.getElementById('forPicSix')
+      recentImg6.style.display = 'none'
+      arrayofTablesId.for6.style.backgroundImage = `
+      url("cf/6.JPG")`
+      arrayofTablesId.for6.style.backgroundRepeat = `no-repeat`
+      arrayofTablesId.for6.style.backgroundSize = `100% 100%`
+      // for6.style.backgroundColor = colors[currentColorIndex]
     }
     // backgroundToBlink.style.backgroundColor = colors[currentColorIndex]
     currentColorIndex = (currentColorIndex + 1) % colors.length;
@@ -136,6 +176,41 @@ const dispLuck = ()=>{
 
   setTimeout(()=>{
     clearInterval(blinkingInterval);
+    if(diceRandNoPlayerOnePlayerOne == 1){
+      let recentImg = document.getElementById('forPicOne')
+      recentImg.style.display = 'block'
+      arrayofTablesId.for1.style.background = `linear-gradient( 120deg, rgba(180, 212, 26, ${colors[currentColorIndex]}), rgba(212, 23, 23, ${colors[currentColorIndex]}), rgba(29, 19, 19, ${colors[currentColorIndex]})),
+      url("cf/1.JPG")`
+      arrayofTablesId.for1.style.backgroundRepeat = `no-repeat`
+      arrayofTablesId.for1.style.backgroundSize = `100% 100%`
+      // arrayofTablesId.for1.style.backgroundColor = colors[currentColorIndex]
+    }else if(diceRandNoPlayerOnePlayerOne == 2){
+      let recentImg2 = document.getElementById('forPicTwo')
+      recentImg2.style.display = 'block'
+      arrayofTablesId.for2.style.background = `linear-gradient( 120deg, rgba(15, 15, 15, ${colors[currentColorIndex]}), rgba(212, 23, 23, ${colors[currentColorIndex]}), rgba(29, 19, 19, ${colors[currentColorIndex]})),
+      url("cf/2.JPG")`
+      arrayofTablesId.for2.style.backgroundRepeat = `no-repeat`
+      arrayofTablesId.for2.style.backgroundSize = `100% 100%`
+      // arrayofTablesId.for2.style.backgroundColor = colors[currentColorIndex]
+    }else if(diceRandNoPlayerOnePlayerOne == 3){
+      let recentImg3 = document.getElementById('forPicThree')
+      recentImg3.style.display = 'block'
+      arrayofTablesId.for3.style.background = `linear-gradient( 120deg, rgba(4, 59, 92, ${colors[currentColorIndex]}), rgba(212, 23, 23, ${colors[currentColorIndex]}), rgba(11, 127, 171, ${colors[currentColorIndex]}, rgba(30, 139, 195, ${colors[currentColorIndex]})),
+      url("cf/3.JPG")`
+      arrayofTablesId.for3.style.backgroundRepeat = `no-repeat`
+      arrayofTablesId.for3.style.backgroundSize = `100% 100%`
+      // for3.style.backgroundColor = colors[currentColorIndex]
+    }else if(diceRandNoPlayerOnePlayerOne == 4){
+      let recentImg4 = document.getElementById('forPicFour')
+      recentImg4.style.display = 'block'
+      // for4.style.backgroundColor = colors[currentColorIndex]
+    }else if(diceRandNoPlayerOnePlayerOne == 5){
+      let recentImg5 = document.getElementById('forPicFive')
+      recentImg5.style.display = 'block'
+    }else if(diceRandNoPlayerOnePlayerOne == 6){
+      let recentImg6 = document.getElementById('forPicSix')
+      recentImg6.style.display = 'block'
+    }
   }, 8000)
 
 };
